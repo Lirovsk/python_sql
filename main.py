dce84211ffa86b6e343c3ade1d7a5631b155dbc1
João Vítor Lira <16088480602@estudante.ufjf.br>
2025-02-27 20:03:11 -0300

    Creating the main.py logic to interact whith th e database

---
+++ main.py
@@ -0,0 +1,16 @@
+import time 
+import os
+from services import *
+from models import Membro, area_hist, charge_hist, base, engine
+from sqlalchemy.orm import Session
+
+base.metadata.create_all(engine)
+
+#create a session
+session = Session(engine)
+
+#crate a new member
+obj_member = add_membro.adding_process()
+instance_member = obj_member.create_isntance()
+session.add(instance_member)
+session.commit()
